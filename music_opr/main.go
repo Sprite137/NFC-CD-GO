3734a2ef0b7747748527ea25c36dff91ccf7b23f
xuzhi <zhizxu@tencent.com>
2024-07-31 19:15:57 +0800

    根据输入处理播放器的播放和暂停

--- music_opr/main.go
+++ music_opr/main.go
@@ -27,15 +27,48 @@ func main() {
 	currentIndex := 0
 	go func() {
 		for {
-			time.Sleep(1 * time.Second)
-			fmt.Print(player.currentPosition(), "\n")
+			if player.ctrl.Paused != true {
+				time.Sleep(1 * time.Second)
+				fmt.Print(player.currentPosition(), "\n")
+			}
 			// 当前音乐播放完，切换下一首
 			if player.isDone() {
 				fmt.Println("正在切换下一首...")
 				player.changeSong(&currentIndex)
-				currentIndex++
 			}
 		}
+	}()
+
+	opr := make(chan int)
+
+	// 起协程获取opr
+	go func() {
+		for {
+			//fmt.Println("请输入操作：")
+			//fmt.Println("0. 暂停/播放")
+			//fmt.Println("1. 下一首")
+			//fmt.Println("2. 上一首")
+			//fmt.Println("3. 退出")
+			//fmt.Println("输入")
+			n, _ := fmt.Scanln()
+			opr <- n
+		}
+	}()
+
+	go func() {
+		for {
+			n := <-opr
+			switch n {
+			case 0:
+				player.togglePlay()
+				if player.ctrl.Paused {
+					fmt.Printf("paused...\n")
+				} else {
+					fmt.Printf("playing...\n")
+				}
+			}
+		}
+
 	}()
 
 	select {}
