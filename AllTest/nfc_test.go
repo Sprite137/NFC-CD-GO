3a1f54e6193d0d79c44adbcfb392fc131a52ac64
Sprite137 <3304424497@qq.com>
2024-08-04 22:22:55 +0800

    尝试读取NFC卡的信息

--- AllTest/nfc_test.go
+++ AllTest/nfc_test.go
@@ -3,6 +3,7 @@ package AllTest
 import (
 	"fmt"
 	"github.com/clausecker/nfc/v2"
+	"log"
 	"testing"
 )
 
@@ -30,13 +31,20 @@ func TestNFC(t *testing.T) {
 	//} else {
 	//	fmt.Println("未检测到 NFC 标签")
 	//}
-	//fmt.Println(nfc.Version())
-	devices, err := nfc.ListDevices()
+	// 打开 NFC 设备
+	dev, err := nfc.Open("")
 	if err != nil {
-		fmt.Print(err)
-		return
+		log.Fatalf("无法打开NFC设备: %v", err)
 	}
-	for _, d := range devices {
-		fmt.Printf("found device %s", d)
+	defer dev.Close()
+
+	// 读取卡片数据，这里以读取第一个块为例
+	data, err := dev.Information()
+
+	if err != nil {
+		log.Fatalf("读取卡片数据失败: %v", err)
 	}
+
+	// 打印读取到的数据
+	fmt.Printf("读取到的 NFC 卡数据: % X\n", data)
 }
--- AllTest/txt2dump_test.go
+++ AllTest/txt2dump_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestTxt2dump(t *testing.T) {
+func TestDump2Txt(t *testing.T) {
 	dumpFilePath := "22.dump"   // M1卡的dump文件路径
 	txtFilePath := "output.txt" // 输出的txt文件路径
 	myutil.Dump2txt(txtFilePath, dumpFilePath)
