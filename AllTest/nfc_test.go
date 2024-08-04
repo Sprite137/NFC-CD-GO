e0de229012546209d0e7de3abaa53c3eec5842c1
Sprite137 <3304424497@qq.com>
2024-08-04 22:22:55 +0800

    尝试读取NFC卡的信息

--- AllTest/nfc_test.go
+++ AllTest/nfc_test.go
@@ -3,17 +3,48 @@ package AllTest
 import (
 	"fmt"
 	"github.com/clausecker/nfc/v2"
+	"log"
 	"testing"
 )
 
 func TestNFC(t *testing.T) {
 	//fmt.Println(nfc.Version())
-	devices, err := nfc.ListDevices()
+	//devices, err := nfc.ListDevices()
+	//if err != nil {
+	//	fmt.Print(err)
+	//	return
+	//}
+	//for _, d := range devices {
+	//	fmt.Printf("found device %s", d)
+	//}
+	//// 检测 NFC 标签
+	//if nfcDev. {
+	//	// 读取 NFC 标签
+	//	tag, err := nfcDev.Read()
+	//	if err != nil {
+	//		fmt.Printf("读取 NFC 标签失败: %v\n", err)
+	//		return
+	//	}
+	//
+	//	// 显示标签信息
+	//	fmt.Printf("检测到 NFC 标签: %+v\n", tag)
+	//} else {
+	//	fmt.Println("未检测到 NFC 标签")
+	//}
+	// 打开 NFC 设备
+	dev, err := nfc.Open("")
 	if err != nil {
-		fmt.Print(err)
-		return
+		log.Fatalf("无法打开NFC设备: %v", err)
 	}
-	for _, d := range devices {
-		fmt.Printf("found device %s", d)
+	defer dev.Close()
+
+	// 读取卡片数据，这里以读取第一个块为例
+	data, err := dev.Information()
+
+	if err != nil {
+		log.Fatalf("读取卡片数据失败: %v", err)
 	}
+
+	// 打印读取到的数据
+	fmt.Printf("读取到的 NFC 卡数据: % X\n", data)
 }
