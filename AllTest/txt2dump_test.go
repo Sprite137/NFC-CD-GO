2a64d364e6dd6f9fbea13450226313ce04f9eda5
xuzhi <zhizxu@tencent.com>
2024-08-04 14:49:45 +0800

    解析mct文件

--- AllTest/txt2dump_test.go
+++ AllTest/txt2dump_test.go
@@ -4,6 +4,17 @@ import (
 	"testing"
 )
 
+const (
+	NDEF_RECORD_HEADER_LEN = 3
+)
+
+type NDEFRecord struct {
+	Header  uint8
+	TypeLen uint8
+	IDLen   uint8
+	Payload []byte
+}
+
 func TestTxt2dump(t *testing.T) {
-	//
+
 }
