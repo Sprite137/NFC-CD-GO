0a6334ba34489097a1208ee1756199004c36d108
Sprite137 <57805811+Sprite137@users.noreply.github.com>
2024-08-04 22:35:01 +0800

    Dev/zhizxu (#4)
    * beep的使用
    
    * 实现播放器的顺序播放
    
    * 实现播放器的顺序播放
    
    * 实现播放器的顺序播放
    
    * 实现播放器的顺序播放
    
    * 根据输入处理播放器的播放和暂停
    
    * 修复bug:(歌曲播放完isDone返回false)
    
    * 完成切换下一首的功能
    
    * 完成切换上一首的功能
    
    * 完成读取歌单配置切换的功能：需要修改bug
    
    * 完成读取歌单配置切换的功能
    
    * 完成读取歌单配置切换的功能
    
    * 完成读取歌单配置切换的功能
    
    * 实现进度条功能
    
    * 修复进度条显示时间错误bug
    
    * 完成循环功能
    
    * todo更新
    
    * 播放逻辑切换
    
    * 完成播放逻辑切换
    
    * 完成web请求模拟nfc
    
    * 完成web请求模拟nfc切换歌单
    
    * 完成web请求模拟nfc切换歌单
    
    * dump文件和txt互转
    
    * dump文件和txt互转
    
    * 只适用于linux，不适用windows和mac
    
    * nfc库可用
    
    * 当txt音乐文件不在本地时，跳过播放切换
    
    * 读取本地所有歌曲
    
    * 读取本地所有歌曲初始化allSongList.txt
    
    * 读取本地所有歌曲初始化allSongList.txt
    
    * 读取本地所有歌曲初始化allSongList.txt
    
    * nfc包
    
    * 编译nfc
    
    * ???
    
    * ???
    
    * nfc包已跑通
    
    * test
    
    * test 路径
    
    * test 路径
    
    * 解决路径问题
    
    * update README.md
    
    * 更新播放的播完逻辑
    
    * 解析mct文件
    
    * 完成dump2txt的逻辑
    
    * 尝试读取NFC卡的信息
    
    * dump文件与txt互转完成
    
    * dump文件与txt互转完成
    
    * beep的使用
    
    * 实现播放器的顺序播放
    
    * 实现播放器的顺序播放
    
    * 实现播放器的顺序播放
    
    * 实现播放器的顺序播放
    
    * 根据输入处理播放器的播放和暂停
    
    * 修复bug:(歌曲播放完isDone返回false)
    
    * 完成切换下一首的功能
    
    * 完成切换上一首的功能
    
    * 完成读取歌单配置切换的功能：需要修改bug
    
    * 完成读取歌单配置切换的功能
    
    * 完成读取歌单配置切换的功能
    
    * 完成读取歌单配置切换的功能
    
    * 实现进度条功能
    
    * 修复进度条显示时间错误bug
    
    * 完成循环功能
    
    * todo更新
    
    * 播放逻辑切换
    
    * 完成播放逻辑切换
    
    * 完成web请求模拟nfc
    
    * 完成web请求模拟nfc切换歌单
    
    * 完成web请求模拟nfc切换歌单
    
    * dump文件和txt互转
    
    * dump文件和txt互转
    
    * 只适用于linux，不适用windows和mac
    
    * nfc库可用
    
    * 当txt音乐文件不在本地时，跳过播放切换
    
    * 读取本地所有歌曲
    
    * 读取本地所有歌曲初始化allSongList.txt
    
    * 读取本地所有歌曲初始化allSongList.txt
    
    * nfc包
    
    * 编译nfc
    
    * ???
    
    * ???
    
    * nfc包已跑通
    
    * test
    
    * test 路径
    
    * test 路径
    
    * 解决路径问题
    
    * update README.md
    
    * 更新播放的播完逻辑
    
    * 完成dump2txt的逻辑
    
    * 解析mct文件
    
    * 尝试读取NFC卡的信息
    
    * dump文件与txt互转完成
    
    * dump文件与txt互转完成
    
    ---------
    
    Co-authored-by: xuzhi <zhizxu@tencent.com>

--- AllTest/nfc_test.go
+++ AllTest/nfc_test.go
@@ -1,13 +1,11 @@
 package AllTest
 
 import (
-	"fmt"
-	"github.com/clausecker/nfc/v2"
 	"testing"
 )
 
 func TestNFC(t *testing.T) {
-	fmt.Println(nfc.Version())
+	//fmt.Println(nfc.Version())
 	//devices, err := nfc.ListDevices()
 	//if err != nil {
 	//	fmt.Print(err)
@@ -30,4 +28,20 @@ func TestNFC(t *testing.T) {
 	//} else {
 	//	fmt.Println("未检测到 NFC 标签")
 	//}
+	// 打开 NFC 设备
+	//dev, err := nfc.Open("")
+	//if err != nil {
+	//	log.Fatalf("无法打开NFC设备: %v", err)
+	//}
+	//defer dev.Close()
+	//
+	//// 读取卡片数据，这里以读取第一个块为例
+	//data, err := dev.Information()
+	//
+	//if err != nil {
+	//	log.Fatalf("读取卡片数据失败: %v", err)
+	//}
+	//
+	//// 打印读取到的数据
+	//fmt.Printf("读取到的 NFC 卡数据: % X\n", data)
 }
--- AllTest/txt2dump_test.go
+++ AllTest/txt2dump_test.go
@@ -1,9 +1,18 @@
 package AllTest
 
 import (
+	myutil "example.com/m/util"
 	"testing"
 )
 
-func TestTxt2dump(t *testing.T) {
-	//
+func TestDump2Txt(t *testing.T) {
+	dumpFilePath := "output_dump.dump" // M1卡的dump文件路径
+	txtFilePath := "output_txt.txt"    // 输出的txt文件路径
+	myutil.Dump2txt(txtFilePath, dumpFilePath)
+}
+
+func TestTxt2Dump(t *testing.T) {
+	dumpFilePath := "output_dump.dump" // M1卡的dump文件路径
+	txtFilePath := "songs.txt"         // 输出的txt文件路径
+	myutil.Txt2dump(txtFilePath, dumpFilePath)
 }
