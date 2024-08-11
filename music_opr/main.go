066f7a627c8181938d4d6b6b7a9f7ac218387a66
Sprite137 <3304424497@qq.com>
2024-08-12 00:47:45 +0800

    修复bug

--- music_opr/main.go
+++ music_opr/main.go
@@ -204,7 +204,9 @@ func main() {
 			oprWeb := <-oprNFCChan
 			switch {
 			case strings.Contains(Uid2SOngListMap[oprWeb], "更换专辑"):
-				player.currentPlayList.SetList(strings.Split(Uid2SOngListMap[oprWeb], ":")[1])
+				if player.currentPlayList.SetList(strings.Split(Uid2SOngListMap[oprWeb], ":")[1]) {
+					fmt.Printf("切换歌单失败 \n")
+				}
 				fmt.Printf("准备切换歌单为%v\n", strings.Split(Uid2SOngListMap[oprWeb], ":")[1])
 				if player.currentPlayList.SongNames != nil {
 					currentIndex = -1
